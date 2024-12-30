affc55cd4bd7cbda10902d0b435b0638cea2e392
asad <asadbahtiyarov2002@gmail.com>
2024-12-30 19:35:16 +0500

    stable

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,24 +1,24 @@
-import {Text, TouchableOpacity, Image, View } from 'react-native'
+import { Text, TouchableOpacity, Image, View } from 'react-native'
 import React from 'react'
 import { gifs } from '../constants'
 
-const CustomButton = ({title, handlePress, containerStyles, textStyles, isLoading}) => {
+const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoading }) => {
   var content = null;
   if (isLoading) {
     content = <View className="w-14 h-14 flex items-center justify-center">
       <Image source={gifs.loading} className="w-full h-full"
-      resizeMode="contain"/>
+        resizeMode="contain" />
     </View>
   } else {
     content = <Text className={`text-primary font-semibold text-lg ${textStyles}`}>
-    {title}
+      {title}
     </Text>
   }
   return (
     <TouchableOpacity
-    onPress={handlePress}
-    activeOpacity={0.7}
-    className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading? 'opacity-50': ''}`} disabled={isLoading}>
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`} disabled={isLoading}>
       {content}
     </TouchableOpacity>
   )
